341ed55811e8bdf758bbef36a471fbf338821cc8
n0bisuke <rsksound@gmail.com>
2015-09-21 04:34:35 +0900

    add logging func by milkcocoa

---
+++ README.md
@@ -0,0 +1,15 @@
+## about
+
+Node.jsで人感センサーとHueの連携
+
+## install
+
+```
+npm i
+```
+
+## start
+
+```
+node jinkan-hue
+```
--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var m = require('mraa');
-var ctrlHue = require('./hue');
+var ctrlHue = require('./lib/hue');
+var logging = require('./lib/logging');
 var myDigitalPin = new m.Gpio(13);
 var motion = false; //人感センサ
 var stateFlag = [false,false]; //人感センサの状態保存
 var HUEID = 4; //操作するHueのID
-
+var ditectCount = 0; //時間毎の検知回数
 myDigitalPin.dir(m.DIR_IN);
 
 function periodicActivity(){
-	var myDigitalValue =  myDigitalPin.read();
-	// console.log('Gpio is ' + myDigitalValue);
+	var date = new Date(); //時間を扱う
+	if(date.getSeconds() === 0){ //時間が来たら保存して初期化
+		logging.save(ditectCount);
+		ditectCount = 0;
+	}
 
+	var myDigitalValue =  myDigitalPin.read();
   if(myDigitalValue){
     stateFlag[1] = true;
     motion = true;
@@ -19,9 +24,11 @@ function periodicActivity(){
     motion = false;
   }
 
+	//状態が変化したときだけHueにHTTPリクエストを飛ばす
   if(stateFlag[0] !== stateFlag[1]){
     ctrlHue(HUEID,motion);
     stateFlag[0] = stateFlag[1];
+		ditectCount++;
   }
 
 	setTimeout(periodicActivity,1000);
