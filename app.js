4263ed85f10c0f0f1726b1850dfd4419cde1d27d
n0bisuke <rsksound@gmail.com>
2015-11-15 12:10:05 +0900

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var m = require('mraa');
-var ctrlHue = require('./lib/hue');
+//var ctrlHue = require('./lib/hue');
 var logging = require('./lib/logging');
 var myDigitalPin = new m.Gpio(13);
 var motion = false; //人感センサ
@@ -7,9 +7,10 @@ var stateFlag = [false,false]; //人感センサの状態保存
 var HUEID = 4; //操作するHueのID
 var ditectCount = 0; //時間毎の検知回数
 myDigitalPin.dir(m.DIR_IN);
-console.log();
+
 function periodicActivity(){
 	var date = new Date(); //時間を扱う
+
 	if(date.getSeconds() === 0){ //時間が来たら保存して初期化
 		logging.save(ditectCount);
 		ditectCount = 0;
@@ -26,8 +27,9 @@ function periodicActivity(){
 
 	//状態が変化したときだけHueにHTTPリクエストを飛ばす
   if(stateFlag[0] !== stateFlag[1]){
-    ctrlHue(HUEID,motion);
+    // ctrlHue(HUEID,motion);
     stateFlag[0] = stateFlag[1];
+		if(stateFlag) logging.send();//点灯時
 		ditectCount++;
   }
 
--- hikari.js
+++ hikari.js
@@ -1,5 +1,14 @@
 import mraa from 'mraa';
 import MilkCocoa from 'milkcocoa';
+let pubnub = require("pubnub")({
+    ssl           : true,  // <- enable TLS Tunneling over TCP
+    publish_key   : "pub-c-8acbf410-aa4e-4f81-ac62-cc94b268ccda",
+    subscribe_key : "sub-c-9705329c-dcc7-11e4-8fb9-0619f8945a4f"
+});
+
+import dweetClient from 'node-dweetio';
+let dweetio = new dweetClient();
+
 //import Milkcocoa from './lib/mlkccaController';
 let milkcocoa = new MilkCocoa("noteifpi0dt2.mlkcca.com");
 let ds = milkcocoa.dataStore('hikari');
@@ -8,10 +17,36 @@ let light = new mraa.Aio(0);
 function lightRead(){
 	let lightValue = light.read();
   lightValue = 50 - lightValue;
+  // if(lightValue < 0) return;
+  // pubnub(lightValue);
+
+  dweetio.dweet_for("lig_hikari", {v:lightValue}, (err, dweet) => {
+    console.log(dweet.thing); // "my-thing"
+    console.log(dweet.content); // The content of the dweet
+    console.log(dweet.created); // The create date of the dweet
+  });
+
+  pubnub.publish({
+      channel   : 'lig_hikari',
+      message   : { "hikari" : lightValue},
+      callback  : (e) => { console.log( "SUCCESS!", e ); },
+      error     : (e) => { console.log( "FAILED! RETRY PUBLISH!", e ); }
+  });
+
   ds.push({v:lightValue},(err,pushed)=>{
     console.log(pushed);
+
     setTimeout(lightRead, 10000);
   })
 }
 
 lightRead();
+
+function pubnub(val){
+  pubnub.publish({
+      channel   : 'lig_hikari',
+      message   : { "hikari" : val},
+      callback  : (e) => { console.log( "SUCCESS!", e ); },
+      error     : (e) => { console.log( "FAILED! RETRY PUBLISH!", e ); }
+  });
+}
